7bee686b7d92fac8ebc156970df77fd261f29161
beesaiyvoud <laoseasybee@gmail.com>
2024-05-18 13:38:26 +0700

    add

--- src/service/service.js
+++ src/service/service.js
@@ -7,11 +7,11 @@ export const GeneratePassword = async (password) => {
 };
 export const GenerateToken = async (data) => {
   var paylod = {
-    id: await encrypt(data.id),
+    id: await encrypt(data.id.toString()),
     role: await encrypt(data.role),
   };
   var paylod_refresh = {
-    id: await encrypt(data.id),
+    id: await encrypt(data.id.toString()),
     role: await encrypt(paylod.role),
   };
   const token = jwt.sign(paylod, SecreatKey, { expiresIn: "2h" });
@@ -22,11 +22,11 @@ export const GenerateToken = async (data) => {
   return { token, refreshToken };
 };
 export const encrypt = async (data) => {
-  const encrypt = CryptoJS.AES.encrypt(data, SECREAT_KEY).toString();
+  const encrypt = CryptoJS.AES.encrypt(data, SecreatKey).toString();
   return encrypt;
 };
 export const Decrypt = async (data) => {
-  const decrypt = CryptoJS.AES.decrypt(data, SECREAT_KEY);
+  const decrypt = CryptoJS.AES.decrypt(data, SecreatKey);
   const result = decrypt.toString(CryptoJS.enc.Utf8);
   return result;
 };
