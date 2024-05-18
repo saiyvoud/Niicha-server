29e7081d67ce76c6602d517370237c57fe6752ff
beesaiyvoud <laoseasybee@gmail.com>
2024-05-18 14:34:44 +0700

    add

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,7 +1,11 @@
 import con from "../config/db.js";
 import { Role, SMessage } from "../service/message.js";
 import { SendCreate, SendError, SendSuccess } from "../service/response.js";
-import { Decrypt, GeneratePassword, GenerateToken } from "../service/service.js";
+import {
+  Decrypt,
+  GeneratePassword,
+  GenerateToken,
+} from "../service/service.js";
 import { ValidateData } from "../service/validate.js";
 
 export default class UserController {
@@ -23,13 +27,13 @@ export default class UserController {
         if (result.length > 0) {
           return SendError(res, 400, "username is already!");
         }
-        const genPassword = await GeneratePassword(password)
+        const genPassword = await GeneratePassword(password);
         const insert =
           "insert into user (username,password,role,createdAt,updatedAt) values (?,?,?,?,?)";
         con.query(
           insert,
           [username, genPassword, Role.employee, datetime, datetime],
-         async function (error, data) {
+          async function (error, data) {
             if (error) throw error;
             return SendCreate(res, SMessage.Register);
           }
@@ -39,43 +43,46 @@ export default class UserController {
       return SendError(res, 500, "ErrorServer Internal", error);
     }
   }
-  static  login(req, res)  {
+  static login(req, res) {
     try {
       const { username, password } = req.body;
       const validate = ValidateData({ username, password });
       if (validate.length > 0) {
         return SendError(res, 400, "Please input: " + validate.join(","));
       }
-      var datetime = new Date()
-        .toISOString()
-        .replace(/T/, " ") // replace T with a space
-        .replace(/\..+/, "");
+      // var datetime = new Date()
+      //   .toISOString()
+      //   .replace(/T/, " ") // replace T with a space
+      //   .replace(/\..+/, "");
 
       const mysql = "Select * from user where username=?";
       con.query(mysql, username, async function (err, result) {
         if (err) throw err;
-        if (result === null || result === undefined || result.length > 0) {
-          return res.status(404).json({ success: false, message: "Not Found" });
+
+        if (result[0] === null || result[0] === undefined) {
+          return res
+            .status(404)
+            .json({ success: false, message: "username and password invaild" });
         }
-        const decryptPassword = await Decrypt(result[0]['password'])
+        const decryptPassword = await Decrypt(result[0]["password"]);
 
         if (password != decryptPassword) {
-          return SendError(res, 401,"Password Not Match");
+          return SendError(res, 401, "Password Not Match");
         }
-        
-        var data ={
-            id: result[0]["UID"],
-            role: Role.employee
-        }
+
+        var data = {
+          id: result[0]["UID"],
+          role: Role.employee,
+        };
         const token = await GenerateToken(data);
         const newData = Object.assign(
-            JSON.parse(JSON.stringify(result[0])),
-            JSON.parse(JSON.stringify(token))
+          JSON.parse(JSON.stringify(result[0])),
+          JSON.parse(JSON.stringify(token))
         );
-        return SendSuccess(res,SMessage.Login,newData)
+        return SendSuccess(res, SMessage.Login, newData);
       });
     } catch (error) {
-        return SendError(res, 500, "ErrorServer Internal", error);
+      return SendError(res, 500, "ErrorServer Internal", error);
     }
   }
 }
