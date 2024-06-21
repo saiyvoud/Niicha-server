74cc8b5d6fc1b0d9bf413af850ed7f2604e950e3
[southixa phlavong] <[southixa.pele10@gmail.com]>
2024-06-22 00:16:20 +0700

    feat: cur product_type, product

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import express from "express"; // ES
 import  "./config/db.js";
 import routes from "./router/route.js";
+import fileUpload from "express-fileupload";
 import cors from "cors";
 import bodyParser from "body-parser";
 const app = express();
 
 
 app.use(cors());
+app.use(fileUpload());
 app.use(bodyParser.json({ extended: true }));
 app.use(
   bodyParser.urlencoded({ 
